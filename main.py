efdfeda577f22bb7752ab1d74917580b647bc5c8
Peter Huynh <peterhyh@yahoo.com>
2023-12-09 17:46:48 -0600

    pandas imported

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import csv
-
-with open("./weather_data.csv") as content:
-    data = csv.reader(content)
-    temperatures = []
-    for row in data:
-        for item in row:
-            if item.isdigit():
-                temperatures.append(int(item))
-    print(temperatures)
-
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
