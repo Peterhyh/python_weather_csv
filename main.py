13828ed497310f9bcfbe9880ec3f486d612899a8
Peter Huynh <peterhyh@yahoo.com>
2023-12-11 15:04:19 -0600

    average temp implemented

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ data = pandas.read_csv("weather_data.csv")
 
 data_dict = data.to_dict()
 
-print(data_dict)
+temp_list = data["Temperature"].to_list()
+
+
+total = 0
+
+for temp in temp_list:
+    total += temp
+
+average_temp = total//len(temp_list)
+
+print(average_temp)
